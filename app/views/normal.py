e1cc8588e536f816a1f8e2676fa945ffb5712aaf
jayakrishnancn <jayakrishnancn@gmail.com>
2019-10-09 22:07:54 +0530

    user order-history completed

--- app/views/normal.py
+++ app/views/normal.py
@@ -1,4 +1,4 @@
-
+import copy
 from django.db.utils import IntegrityError
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
@@ -16,7 +16,21 @@ def orderHistory(request):
         messages.error(request,"cant save. please try again or conact admin")
         return redirect('order_history')
 
-    data['orders'] = OrderedItem.objects.filter(order__user=request.user)
+    ordersFromDb = Order.objects.filter(user=request.user).order_by('-id')
 
-    print(data)
+    newOrders = []
+    for order in ordersFromDb:
+        tmp_order = {}
+        tmp_order['id'] = order.id
+        tmp_order['delivered'] = order.delivered
+        tmp_order['rider'] = order.rider
+        tmp_order['deliveredOn'] = order.deliveredOn
+        tmp_order['total'] = order.total_price 
+        tmp_order['items'] = OrderedItem.objects.filter(order=order).order_by('-id')
+        newOrders.append(tmp_order)
+
+    print(newOrders)
+
+    data['orders'] = newOrders
+
     return render(request,'partner/owner/order_history.html',processData(request,data))
